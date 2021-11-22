f370c978266cf1c4d8712a9bb99a680f2dc84745
inspecticode <94567898+inspecticode@users.noreply.github.com>
2021-11-22 19:09:33 +0100

    Add files via upload

---
+++ 3ème degrés 1 racine.py
@@ -0,0 +1,16 @@
+import time
+
+b = int(input("b = "))
+
+x = int(input("x = "))
+y = int(input("y = "))
+
+ax = y - b
+
+x3 = x * x * x
+
+a = ax / x3
+
+print("f(x)=ax3+b")
+print("avec a =", a)
+time.sleep(3)
